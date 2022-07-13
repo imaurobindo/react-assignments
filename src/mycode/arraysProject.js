9e24a0b05248a653aeac7a484c4cb7970ddd3995
Aurobindo <aurobindosardar@gmail.com>
2022-07-13 19:19:39 +0530

    Added styles and also Conditional rendering to buttons and styles

--- src/mycode/arraysProject.js
+++ src/mycode/arraysProject.js
@@ -34,19 +34,19 @@ export default class ArraysProject extends React.Component {
         this.setState({
             tasksComp: temp1
         })
-        
+
     }
 
     handleComplete = (single, index) => {
         var temp = this.state.tasksArr;
         temp.splice(index, 1);
-        
+
         this.setState({
             tasksArr: temp
         })
     }
 
-   
+
 
     render() {
         return (
@@ -56,48 +56,86 @@ export default class ArraysProject extends React.Component {
                     onChange={this.handleInput}
                 />
                 <button
+                    disabled={this.state.taskInput === "" ? true : false}
                     onClick={this.pushTask}
                 >
                     Add task
                 </button>
-
-                <h1>Tasks to do</h1>
-                <div>
-                    <ol>
-                        {this.state.tasksArr.map((single, index) => {
-                            return (
-                                <li>
-                                    {single}
-                                    <button
-                                        onClick={(e) => {
-                                            this.pushComp(single, index);
-                                            this.handleComplete(single, index);
-                                            
-                                            
-                                        }}
-                                    >
-                                        Completed
-                                    </button>
-                                </li>
-                            )
-                        })}
-                    </ol>
-                    <br />
-                    <br />
-
-                    <h1>Tasks Completed</h1>
+                {this.state.tasksArr.length === 0 ?
+                    <h1>You have no tasks in your list</h1>
+                    :
+                    <h1>Tasks to do</h1>
+                }
+                {this.state.tasksArr.length === 0 ?
                     <div>
+                        <br />
+                        <br />
+                    </div>
+                    :
+                    <div
+                        style={{
+                            backgroundColor: "#f2f2f2",
+                            padding: 20,
+                            borderRadius: 10,
+                            width: 200
+                        }}
+                    >
                         <ol>
-                            {this.state.tasksComp.map((single, index) => {
+                            {this.state.tasksArr.map((single, index) => {
                                 return (
                                     <li>
                                         {single}
+                                        <button
+                                            onClick={(e) => {
+                                                this.pushComp(single, index);
+                                                this.handleComplete(single, index);
 
+
+                                            }}
+                                        >
+                                            Completed
+                                        </button>
                                     </li>
                                 )
                             })}
                         </ol>
+                        <br />
+                        <br />
                     </div>
+                }
+                <div>
+
+                    {this.state.tasksComp.length === 0 ?
+                        <h1>No tasks have been completed</h1>
+                        :
+                        <h1>Completed Tasks</h1>
+                    }
+                    {this.state.tasksComp.length === 0 ?
+                        <div>
+                            <br />
+                            <br />
+                        </div>
+                        :
+                        <div
+                            style={{
+                                backgroundColor: "#f2f2f2",
+                                padding: 20,
+                                borderRadius: 10,
+                                width: 200
+                            }}
+                        >
+                            <ol>
+                                {this.state.tasksComp.map((single, index) => {
+                                    return (
+                                        <li>
+                                            {single}
+
+                                        </li>
+                                    )
+                                })}
+                            </ol>
+                        </div>
+                    }
                 </div>
             </div>
         )
